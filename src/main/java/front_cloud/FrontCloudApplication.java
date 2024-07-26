32cd39121d01dde511680cd68e7d7f38356257d1
Eerang2 <dlwlsdn8022@gmail.com>
2024-07-26 17:17:24 +0900

    commit

---
+++ src/main/java/front_cloud/FrontCloudApplication.java
@@ -0,0 +1,13 @@
+package front_cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontCloudApplication.class, args);
+	}
+
+}
